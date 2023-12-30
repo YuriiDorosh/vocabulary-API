53dc60b2062e0171d327e029e98a7318180889df
Yurii <doroshyuriy2005@gmail.com>
2023-12-31 00:45:49 +0200

    UPDATE: config/urls(me url)

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,7 +9,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('auth/', include('djoser.urls')),
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("users/", include("apps.users.urls")),
 ]
