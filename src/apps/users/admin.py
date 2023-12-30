410f8038c37ea0412884792fef9547390cfacf73
Yurii <doroshyuriy2005@gmail.com>
2023-12-30 21:44:08 +0200

    ADD: users app

---
+++ src/apps/users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from apps.users.models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "first_name", "last_name", "date_added")
+    list_filter = ("id", "username", "email")
+    search_fields = ("username", "email")
