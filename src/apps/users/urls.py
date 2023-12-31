8649b089aac097f76b02dc53d9218dae302fc262
Yurii <doroshyuriy2005@gmail.com>
2023-12-31 12:37:43 +0200

    UPDATE: urls(registration and login)

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from apps.users.api.views import SelfView
+from apps.users.api.views.self_views import SelfView
+from apps.users.api.views.auth_views import UserRegistrationView, TokenObtainPairView
 
 urlpatterns = [
     path("me/", SelfView.as_view()),
+    path("registration/", UserRegistrationView.as_view()),
+    path("login/", TokenObtainPairView.as_view()),
 ]
