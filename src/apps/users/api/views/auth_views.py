02787c6c3051afe457f83c8eff214139ce8f7724
Yurii <doroshyuriy2005@gmail.com>
2023-12-31 15:48:05 +0200

    UPDATE: auth view(registration and login views)

--- src/apps/users/api/views/auth_views.py
+++ src/apps/users/api/views/auth_views.py
@@ -1,28 +1,50 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.views import TokenObtainPairView
 
-from apps.users.api.serializers.token_serializers import CustomTokenObtainPairSerializer
-from apps.users.api.serializers.user_serializers import UserSerializer
+from django.contrib.auth import authenticate
+
+from rest_framework.request import Request
+
+from apps.users.api.serializers.user_serializers import UserAuthSerializer
+
+from core.tokens import create_jwt_pair_for_user
 
 
 class UserRegistrationView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
+    def post(self, request: Request):
+        serializer = UserAuthSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            refresh = RefreshToken.for_user(user)
+            user.set_password(request.data.get('password'))
+            user.save()
             return Response(
-                {
-                    "refresh": str(refresh),
-                    "access": str(refresh.access_token),
-                },
+                    data={
+                        "user_id": str(user.id),
+                        "tokens": create_jwt_pair_for_user(user=user),
+                    },
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
+class UserLoginView(APIView):
+
+    def post(self, request: Request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            return Response(
+                data={
+                    "user_id": str(user.id),
+                    "tokens": create_jwt_pair_for_user(user=user),
+                    },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(data={"message": "Invalid username or password"})
+
