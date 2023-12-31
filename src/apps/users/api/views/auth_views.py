b55adcb9ee8134c4d111917454efa1d97e1b0420
Yurii <doroshyuriy2005@gmail.com>
2023-12-31 12:37:54 +0200

    ADD: auth views

---
+++ src/apps/users/api/views/auth_views.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from apps.users.models import User
+from apps.users.api.serializers.user_serializers import UserSerializer
+from apps.users.api.serializers.token_serializers import CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer  
