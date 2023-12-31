07adce751a504d58665863e95052fb745eb07afb
Yurii <doroshyuriy2005@gmail.com>
2023-12-31 12:38:01 +0200

    ADD: self views

---
+++ src/apps/users/api/views/self_views.py
@@ -0,0 +1,17 @@
+from apps.users.api.serializers.user_serializers import UserSerializer
+from apps.users.models import User
+
+
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+
+class SelfView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self) -> User:
+        return self.request.user
+
+    def get_queryset(self):
+        return User.objects.filter(is_active=True)
