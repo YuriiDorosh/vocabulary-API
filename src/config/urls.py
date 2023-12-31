b9dba9022b6e9f762d362938bb07c378b45bfcfd
Yurii <doroshyuriy2005@gmail.com>
2023-12-31 17:01:16 +0200

    UPDATE: urls(path)

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView, TokenVerifyView)
 
+API_PATH = "api/v1/"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("users/", include("apps.users.urls")),
+    path(f"{API_PATH}token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(f"{API_PATH}token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(f"{API_PATH}token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path(f"{API_PATH}users/", include("apps.users.urls")),
 ]
