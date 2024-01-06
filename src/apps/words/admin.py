2087bb1021a391a3d90746497931dbb196c5f001
Yurii <yuriydorosh2005@gmail.com>
2024-01-06 20:33:43 +0200

    UPDATE: admin panel(words app)

--- src/apps/words/admin.py
+++ src/apps/words/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.words.models import Word, Sentence, Topic, TopicWord
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TopicWord)
+class TopicWordAdmin(admin.ModelAdmin):
+    pass
